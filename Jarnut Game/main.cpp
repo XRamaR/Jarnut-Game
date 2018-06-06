0a00df0764c120ff0962c93b594da34c37f5493b
Łukasz Jarnut <lukasz.jarnut@student.put.poznan.pl>
2018-06-06 17:48:24 +0200

    0.06
    small imprivements

--- Jarnut Game/main.cpp
+++ Jarnut Game/main.cpp
@@ -17,12 +17,13 @@ int main()
 	sf::Texture playerTexture;
 	sf::Texture backTexture;
 	sf::Texture wallTexture;
-	sf::Texture keyTexture;
+	sf::Texture platformTexture;
 	sf::Texture endingScreenTexture;
 
 	playerTexture.loadFromFile("droid_from_android.png");
 	backTexture.loadFromFile("background.png");
 	wallTexture.loadFromFile("wall.png");
+	platformTexture.loadFromFile("plank.png");
 	endingScreenTexture.loadFromFile("Endingscreen.png");
 
 	sf::Sprite endingScreen(endingScreenTexture);
@@ -33,18 +34,20 @@ int main()
 
 	Player player(&playerTexture, sf::Vector2u(3, 9), 0.3f, 130.0f, 160.0f);
 
-	platforms.push_back(Platform(NULL, sf::Vector2f(650.0f, 30.0f), sf::Vector2f(320.0f, 470.0f))); //floor
-	//platforms.push_back(Platform(NULL, sf::Vector2f(260.0f, 30.0f), sf::Vector2f(120.0f, 340.0f)));
-	platforms.push_back(Platform(NULL, sf::Vector2f(260.0f, 30.0f), sf::Vector2f(500.0f, 340.0f)));
-	//platforms.push_back(Platform(NULL, sf::Vector2f(240.0f, 30.0f), sf::Vector2f(320.0f, 210.0f)));
-	//platforms.push_back(Platform(NULL, sf::Vector2f(240.0f, 30.0f), sf::Vector2f(120.0f, 100.0f)));
-	//platforms.push_back(Platform(NULL, sf::Vector2f(240.0f, 30.0f), sf::Vector2f(500.0f, 100.0f)));
+	platforms.push_back(Platform(&platformTexture, sf::Vector2f(650.0f, 30.0f), sf::Vector2f(320.0f, 470.0f))); //floor
+	platforms.push_back(Platform(&platformTexture, sf::Vector2f(260.0f, 30.0f), sf::Vector2f(320.0f, 340.0f)));
 
-	for (int i = 0; i < 20; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		platforms.push_back(Platform(NULL, sf::Vector2f(240.0f, 30.0f), sf::Vector2f(320.0f, 80.0f - 200.0f*i)));
-		platforms.push_back(Platform(NULL, sf::Vector2f(240.0f, 30.0f), sf::Vector2f(120.0f, 180.0f - 200.0f*i)));
-		platforms.push_back(Platform(NULL, sf::Vector2f(240.0f, 30.0f), sf::Vector2f(500.0f, 180.0f - 200.0f*i)));
+		platforms.push_back(Platform(&platformTexture, sf::Vector2f(240.0f - 25.0f*i, 30.0f), sf::Vector2f(330.0f, 100.0f - 220.0f*i)));
+		platforms.push_back(Platform(&platformTexture, sf::Vector2f(240.0f - 25.0f*i, 30.0f), sf::Vector2f(120.0f, 190.0f - 220.0f*i)));
+		platforms.push_back(Platform(&platformTexture, sf::Vector2f(240.0f - 25.0f*i, 30.0f), sf::Vector2f(500.0f, 190.0f - 220.0f*i)));
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		platforms.push_back(Platform(&platformTexture, sf::Vector2f(240.0f - 25.0f*(10-i), 30.0f), sf::Vector2f(330.0f, -2100.0f - 220.0f*i)));
+		platforms.push_back(Platform(&platformTexture, sf::Vector2f(240.0f - 25.0f*(10 - i), 30.0f), sf::Vector2f(120.0f, -2010.0f - 220.0f*i)));
+		platforms.push_back(Platform(&platformTexture, sf::Vector2f(240.0f - 25.0f*(10 - i), 30.0f), sf::Vector2f(500.0f, -2010.0f - 220.0f*i)));
 	}
 
 
@@ -90,7 +93,9 @@ int main()
 		player.Draw(window);
 		for (Platform &platform : platforms)
 		{
-			platform.Move(sf::Vector2f(0.0f, 0.05f));
+			sf::Time time = clock.getElapsedTime();
+			float timeAs = time.asMilliseconds;
+			platform.Move(sf::Vector2f(0.0f, 0.05f+timeAs));
 		}
 		for (Platform &platform : platforms)
 		{
